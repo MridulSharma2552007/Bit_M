d051b58b52f268433b4a21784d34ee87b00b6370
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-11-10 01:09:44 +0530

    Refactor Navbar to use StatelessWidget; implement bottom navigation with state management and add curved_navigation_bar dependency

--- lib/WIDGETS/navbar.dart
+++ lib/WIDGETS/navbar.dart
@@ -1,46 +1,55 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class Navbar extends StatefulWidget {
-  const Navbar({super.key});
+class Navbar extends StatelessWidget {
+  final int currentindex;
+  final Function(int) onTapItem;
 
-  @override
-  State<Navbar> createState() => _NavbarState();
-}
+  const Navbar({
+    super.key,
+    required this.currentindex,
+    required this.onTapItem,
+  });
 
-class _NavbarState extends State<Navbar> {
+  final items = const [
+    Icons.home,
+    Icons.search,
+    Icons.favorite_outlined,
+    Icons.settings,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-          child: Container(
-            height: 70,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.white.withOpacity(0.6),
-                width: 1,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.grey.withValues(alpha: 0.2),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(items.length, (i) {
+            final bool isSelected = currentindex == i;
+            return GestureDetector(
+              onTap: () => onTapItem(i),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: isSelected ? Colors.black12 : Colors.transparent,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Icon(
+                  items[i],
+                  size: 28,
+                  color: isSelected ? Colors.black : Colors.grey,
+                ),
               ),
-              color: Colors.black.withOpacity(0.5),
-              borderRadius: BorderRadius.circular(20),
-            ),
-
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [],
-            ),
-          ),
+            );
+          }),
         ),
       ),
     );
   }
-}
-
-Widget _builderIcon(IconData icon, bool isSelected) {
-  return Icon(icon, color: isSelected ? Colors.white : Colors.black, size: 30);
 }
--- lib/roots/root.dart
+++ lib/roots/root.dart
@@ -1,14 +1,41 @@
 import 'package:bit_m/WIDGETS/navbar.dart';
+import 'package:bit_m/roots/home.dart';
+import 'package:bit_m/roots/playlist.dart';
+import 'package:bit_m/roots/search.dart';
+import 'package:bit_m/roots/settings.dart';
 import 'package:flutter/material.dart';
 
-class Root extends StatelessWidget {
+class Root extends StatefulWidget {
   const Root({super.key});
 
   @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  int currentindex = 0;
+  final List<Widget> pages = [Home(), Search(), Playlist(), Settings()];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      bottomNavigationBar: Navbar(),
+      body: Stack(
+        children: [
+          pages[currentindex],
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Navbar(
+              currentindex: currentindex,
+              onTapItem: (index) {
+                setState(() {
+                  currentindex = index;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
