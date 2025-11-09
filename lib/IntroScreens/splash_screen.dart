448123be7d24f57f8556de21301e918603cd5cd5
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-11-09 18:02:29 +0530

    added a cool loading screen and also a root dir and vid player package

--- lib/IntroScreens/splash_screen.dart
+++ lib/IntroScreens/splash_screen.dart
@@ -1,5 +1,6 @@
-import 'package:bit_m/colors/app_colors.dart';
+import 'package:bit_m/roots/root.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,19 +10,35 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('lib/assets/video/loading.mp4');
+    _controller.initialize().then((_) {
+      setState(() {
+        _controller.play();
+        _controller.setLooping(false);
+        _controller.setVolume(0);
+      });
+    });
+    if (!mounted) return;
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Root()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryBgColor,
+      backgroundColor: Colors.black,
       body: Center(
-        child: Text(
-          'Bit_M',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: AppColors.secondaryBgColor,
-          ),
-        ),
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Center(child: CircularProgressIndicator()),
       ),
     );
   }
--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -4,5 +4,5 @@ class AppColors {
   static const Color primaryBgColor = Color(0xff1d1d1f);
   static const Color secondaryBgColor = Color(0xfff5f5f7);
   static const Color primaryBlue = Color(0xff0066cc);
-  static const Color secondaryColor = Color(0x000000ff);
+  static const Color secondaryColor = Color(0xffC9CBC5);
 }
